c8874c817a99c856c8e438dd3b6bcdc037f6daaa
Константин Богданов <bogdakos@mail.ru>
2018-10-03 19:15:13 +0300

    Initialize

---
+++ Flickr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flickr
+//
+//  Created by Константин Богданов on 03.10.2018.
+//  Copyright © 2018 Константин Богданов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
