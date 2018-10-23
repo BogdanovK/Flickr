02a1133b35fff22cdfad4aa3f79342f3ee601a68
TheGreat <helpupd@gmail.com>
2018-10-23 23:05:40 +0300

    Выполнен Серчбар + его инит

---
+++ Flickr/View/SearchBar.swift
@@ -0,0 +1,28 @@
+//
+//  SearchBar.swift
+//  Flickr
+//
+//  Created by Vio on 23.10.2018.
+//  Copyright © 2018 Константин Богданов. All rights reserved.
+//
+
+import UIKit
+
+/**
+ Бар поиска
+ */
+
+class SearchBar: UITextField {
+    
+    var customPlaceHolder: String?
+    
+    init?(withCustomPlaceHolder: String) {
+        super.init(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        self.customPlaceHolder = withCustomPlaceHolder
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+}
