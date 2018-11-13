7a368b17c00b9e714243113164079da3d9f93de2
Kozaderov Ivan <>
2018-11-13 22:12:59 +0300

    Search Bar

--- Flickr/ViewControllers/SettingsViewController.swift
+++ Flickr/ViewControllers/SettingsViewController.swift
@@ -12,24 +12,73 @@ import UIKit
  Контроллер изменения настроек пользователя.
  */
 
-class SettingsViewController: UIViewController, TabBarSetupProtocol {
-    
+class SettingsViewController: UIViewController, TabBarSetupProtocol,UITableViewDelegate,UITableViewDataSource {
+
     let tableView:UITableView
     
     required init?(coder aDecoder: NSCoder) {
-        
-     self.tableView = UITableView(frame: CGRect(x: 0, y: 0, width: 0, height: 0), style: .plain)
+   //  self.tableView = UITableView(frame: CGRect(x: 0, y: 0, width: 0, height: 0), style: .plain)
+     self.tableView = UITableView(frame: CGRect(x: 0, y: 70, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height), style: .plain)
      super.init(coder: aDecoder)
-        
     }
-    
+
     init() {
-        self.tableView = UITableView(frame: CGRect(x: 0, y: 0, width: 0, height: 0), style: .plain)
+//      self.tableView = UITableView(frame: CGRect(x: 0, y: 0, width: 0, height: 0), style: .plain)
+        self.tableView = UITableView(frame: CGRect(x: 0, y: 70, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height), style: .plain)
         super.init(nibName: nil, bundle: nil)
     }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.green
+        setupSearchBar()
+        setupTableView()
+    }
+    
+    
+ //MARK:Search Bar setup
+    
+    func setupSearchBar() {
+
+        let searchBar = SearchBar.init(withCustomPlaceHolder: "")!
+        searchBar.backgroundColor = UIColor.white
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.placeholder = "Поиск в настройках"
+        searchBar.layer.cornerRadius = 10
+        searchBar.textAlignment = NSTextAlignment.center
+        self.view.addSubview(searchBar)
+        
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            searchBar.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor, constant:5),
+            searchBar.rightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.rightAnchor, constant:-5),
+            searchBar.heightAnchor.constraint(equalToConstant: 40)
+            ])
+    }
+//MARK: TableView UITableViewDelegate,UITableViewDataSource,Settings
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell.init(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "Настройка № \(indexPath.row)"
+        return cell
+    }
+
+    func setupTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.view.addSubview(self.tableView)
+        
     }
     
 //MARK: TabBarSetupProtocol
