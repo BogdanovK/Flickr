b590f6b0c574f3fa185c3168a45b8d785d4266b5
Kozaderov Ivan <>
2018-11-14 19:02:10 +0300

    fixed

--- Flickr/ViewControllers/SettingsViewController.swift
+++ Flickr/ViewControllers/SettingsViewController.swift
@@ -17,14 +17,12 @@ class SettingsViewController: UIViewController, TabBarSetupProtocol,UITableViewD
     let tableView:UITableView
     
     required init?(coder aDecoder: NSCoder) {
-   //  self.tableView = UITableView(frame: CGRect(x: 0, y: 0, width: 0, height: 0), style: .plain)
-     self.tableView = UITableView(frame: CGRect(x: 0, y: 70, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height), style: .plain)
-     super.init(coder: aDecoder)
+        self.tableView = UITableView(frame: CGRect(x: 0, y: 0, width: 0, height: 0), style: .plain)
+        super.init(coder: aDecoder)
     }
 
     init() {
-//      self.tableView = UITableView(frame: CGRect(x: 0, y: 0, width: 0, height: 0), style: .plain)
-        self.tableView = UITableView(frame: CGRect(x: 0, y: 70, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height), style: .plain)
+        self.tableView = UITableView(frame: CGRect(x: 0, y: 0, width: 0, height: 0), style: .plain)
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -41,19 +39,18 @@ class SettingsViewController: UIViewController, TabBarSetupProtocol,UITableViewD
     
     func setupSearchBar() {
 
-        let searchBar = SearchBar.init(withCustomPlaceHolder: "")!
-        searchBar.backgroundColor = UIColor.white
-        searchBar.translatesAutoresizingMaskIntoConstraints = false
-        searchBar.placeholder = "Поиск в настройках"
-        searchBar.layer.cornerRadius = 10
-        searchBar.textAlignment = NSTextAlignment.center
-        self.view.addSubview(searchBar)
+        let searchBar = SearchBar.init(withCustomPlaceHolder: "Поиск в настройках")
+        searchBar!.backgroundColor = UIColor.white
+        searchBar!.translatesAutoresizingMaskIntoConstraints = false
+        searchBar!.layer.cornerRadius = 10
+        searchBar!.textAlignment = NSTextAlignment.center
+        self.view.addSubview(searchBar!)
         
         NSLayoutConstraint.activate([
-            searchBar.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
-            searchBar.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor, constant:5),
-            searchBar.rightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.rightAnchor, constant:-5),
-            searchBar.heightAnchor.constraint(equalToConstant: 40)
+            searchBar!.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            searchBar!.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor, constant:5),
+            searchBar!.rightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.rightAnchor, constant:-5),
+            searchBar!.heightAnchor.constraint(equalToConstant: 40)
             ])
     }
 //MARK: TableView UITableViewDelegate,UITableViewDataSource,Settings
@@ -77,8 +74,14 @@ class SettingsViewController: UIViewController, TabBarSetupProtocol,UITableViewD
     func setupTableView() {
         self.tableView.delegate = self
         self.tableView.dataSource = self
+        self.tableView.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(self.tableView)
         
+        NSLayoutConstraint.activate([
+            self.tableView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width),
+            self.tableView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height),
+            self.tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant:50),
+            ])
     }
     
 //MARK: TabBarSetupProtocol
