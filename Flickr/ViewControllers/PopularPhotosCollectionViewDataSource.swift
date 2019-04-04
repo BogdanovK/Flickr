2ff1e9a9aaa3ee559a1d1a9a541dd05fec3dd72a
Kozaderov Ivan <>
2019-04-04 20:55:21 +0300

    fixes

--- Flickr/ViewControllers/PopularPhotosCollectionViewDataSource.swift
+++ Flickr/ViewControllers/PopularPhotosCollectionViewDataSource.swift
@@ -1,21 +1,22 @@
 //
-//  CollectionViewDelegateDataSource.swift
+//  PopularPhotosCollectionViewDataSource.swift
 //  Flickr
 //
-//  Created by Kozaderov Ivan on 15/02/2019.
+//  Created by Kozaderov Ivan on 04/04/2019.
 //  Copyright © 2019 Константин Богданов. All rights reserved.
 //
 
 import UIKit
 
-class CollectionViewDelegateDataSource: NSObject,UICollectionViewDelegate,UICollectionViewDataSource {
-    
+class PopularPhotosCollectionViewDataSource: NSObject,UICollectionViewDataSource{
+
     var models: [ImageModel] = []
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
         return models.count;
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let model = models[indexPath.row]
@@ -23,13 +24,12 @@ class CollectionViewDelegateDataSource: NSObject,UICollectionViewDelegate,UIColl
         var cell = collectionView.dequeueReusableCell(withReuseIdentifier:  CollectionViewCell.reuseIdentifier, for: indexPath) as? CollectionViewCell
         
         if  cell == nil {
-            cell = CollectionViewCell()
+            cell = CollectionViewCell(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
         }
-        
+
         cell?.photoImageView.image = model.image
         cell?.setNeedsUpdateConstraints()
         return cell!
     }
     
-
 }
